1e3621c04ab313954ec3a2294337bef57b2702d8
Sarthak Shah <sharthakshah81173@gmail.com>
2021-02-10 15:57:55 -0500

    sending messages

---
+++ public/scripts/components/ThemessageComponent.js
@@ -0,0 +1,12 @@
+export default {
+    props: ['msg'],
+
+    template:
+    `
+    <article>
+        <h1>This is a message</h1>
+        <h4> {{msg.message.name}} says:</h4>
+        <p>{{msg.message.content}}</p>
+    <article>
+    `
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,47 @@
+import ChatMessage from "./components/ThemessageComponent.js";
+
 (() => {
     console.log('fired');
+
+    const socket = io();
+
+    //messenger servce event handling -> incoming from the manager
+    function setUserId({sID, message}) {
+        // incomung connected event with data
+        debugger;
+        vm.socketID = sID;
+    }
+
+    function appendMessage(message) {
+        vm.messages.push(message);
+    }
+
+    const vm = new Vue({
+        data: {
+            messages: [],
+            nickname: "",
+            username: "",
+            socketID: "",
+            message: ""
+        },
+
+        created: function() {
+            console.log('its alive');
+        },
+
+        methods: {
+            dispatchMessage() {
+                socket.emit('chatmessage', {content: this.message, name: this.nickname || "Anonymous"})
+            }
+
+        },
+
+        components: {
+            newmessage: ChatMessage
+        }
+    }).$mount("#app");
+
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener('message', appendMessage); 
 
 })();
